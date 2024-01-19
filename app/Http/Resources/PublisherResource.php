ebb75c6b8dc351da0060d7d855a1a85a9e619786
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-19 11:01:29 +0900

    PublisherのAPIリソースを実装しました。 #11

---
+++ app/Http/Resources/PublisherResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PublisherResource extends JsonResource
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $data = [
+            'publisherId' => $this->id,
+            'name' => $this->publisher_name,
+        ];
+
+        if ($this->relationLoaded('books')) {
+            $data['books'] = BookResource::collection($this->books);
+        }
+
+        if ($this->relationLoaded('publishers')) {
+            $data['relatedAuthors'] = AuthorResource::collection($this->authors);
+        }
+
+        return $data;
+    }
+}
