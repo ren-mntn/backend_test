2b0475bba504ef29fe5e53d0a5ceb0dd54a07d17
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-20 17:37:12 +0900

    BaseBookRequestから継承するように変更を行いました。 #32

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class StoreBookRequest extends FormRequest
+class StoreBookRequest extends BaseBookRequest
 {
     /**
      * @return bool
@@ -19,31 +17,8 @@ class StoreBookRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'isbn' => 'required|string|isbn13',
-            'name' => 'required|string|max:255',
-            'publishedAt' => 'required|date|date_format:Y-m-d\TH:i:s.u',
-            // 'author' =>
-            // 'publisher' =>
-            // TODO リクエストデータの詳細の回答まち
-        ];
-    }
-
-    /**
-     * @return array<string, string>
-     */
-    public function messages()
-    {
-        return [
-            'isbn.required' => 'ISBNは必須です。',
-            'isbn.string' => 'ISBNは文字列である必要があります。',
-            'isbn.isbn13' => 'ISBNは13桁である必要があります。',
-            'name.required' => '書籍名は必須です。',
-            'name.string' => '書籍名は文字列である必要があります。',
-            'name.max' => '書籍名は最大255文字までです。',
-            'publishedAt.required' => '出版日は必須です。',
-            'publishedAt.date' => '出版日は有効な日付形式である必要があります。',
-            'publishedAt.date_format' => '出版日は "Y-m-d\TH:i:s.u" 形式に一致する必要があります。',
-        ];
+        $rules = parent::commonRules();
+        $rules['isbn'][] = 'unique:books,isbn';
+        return $rules;
     }
 }
