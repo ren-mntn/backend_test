c4c2868b5db3376fb2b84186968132130ea72e53
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-20 17:35:22 +0900

    ビジネスロジックを実装しました。 #32

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -10,20 +10,46 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $isbn
  * @property string $book_name
  * @property string $published_at
+ * @property string $author_id
+ * @property string $publisher_id
  */
 class Book extends Model
 {
     use HasFactory;
 
     protected $primaryKey = 'isbn';
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     protected $fillable = [
         'isbn',
         'book_name',
-        'published_at'
+        'published_at',
+        'author_id',
+        'publisher_id'
     ];
 
-    public $timestamps = false; 
+    public $timestamps = false;
+
+    /**
+     * @param array $data 書籍のデータ
+     * @return self 作成された書籍インスタンス
+     */
+    public static function createBook(array $data)
+    {
+        return self::create($data);
+    }
+
+    /**
+     * @param array $data 更新する書籍のデータ
+     * @return self 更新された書籍インスタンス
+     */
+    public function updateBook(array $data)
+    {
+        $this->fill($data);
+        $this->save();
+        return $this;
+    }
 
     public function author()
     {
@@ -33,5 +59,10 @@ class Book extends Model
     public function publisher()
     {
         return $this->belongsTo(Publisher::class, 'publisher_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'isbn';
     }
 }
