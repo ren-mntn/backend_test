49e16f9bb9a00cbb93dbe68d6c4d32b32ecfbd89
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-21 22:50:19 +0900

    ビジネスロジックを実装しました。 #32

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -20,10 +20,42 @@ class Publisher extends Model
         'publisher_name',
     ];
 
-    public $timestamps = false; 
+    public $timestamps = false;
+
+    /**
+     * @param array $publisherData
+     * @return self 作成された出版社インスタンス
+     */
+    public static function createPublisher(array $publisherData)
+    {
+        return self::create($publisherData);
+    }
+
+    /**
+     * @param array $publisherData
+     * @return self 更新された出版社インスタンス
+     */
+    public function updatePublisher(array $publisherData)
+    {
+        $this->fill($publisherData);
+        $this->save();
+        return $this;
+    }
 
     public function books()
     {
         return $this->hasMany(Book::class, 'publisher_id');
+    }
+
+    public function authors()
+    {
+        return $this->hasManyThrough(
+            Author::class,
+            Book::class,
+            'publisher_id',
+            'id',
+            'id',
+            'author_id'
+        );
     }
 }
